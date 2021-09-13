e462f63f212fd26d81d11eb790bc5048cf779170
7736243 <7736243@naver.com>
2021-09-13 14:19:51 +0900

    20210913 ipc(pipe)

--- pipe.c
+++ pipe.c
@@ -11,24 +11,30 @@ int main(){
     pid_t pid;
     char buf[MAX_BUF];
 
-    if(pipe(fd)<0){ //파이프 호출 성공시 0 반환, 아니면 -1 반환
+    //파이프 호출 성공시 0 반환, 아니면 -1 반환
+    if(pipe(fd)<0){ 
         printf("pipe error !\n"); 
         exit(1);
     }
+
     if((pid=fork())<0){ 
         printf("fork error !\n");
         exit(1);
     }
 
     if(pid>0){
-        close(fd[READ]); // 자식 프로세스는 Write할꺼기에 Read fd는 닫아줌
+        // 자식 프로세스는 Write할꺼기에 Read fd는 닫아줌
+        close(fd[READ]); 
         strcpy(buf, "message from parent\n");
         write(fd[WRITE], buf, strlen(buf));
     }
+
     else{
-        close(fd[WRITE]); // 부모 프로세스는 Read할꺼기에 Write fd는 닫아줌
+        // 부모 프로세스는 Read할꺼기에 Write fd는 닫아줌
+        close(fd[WRITE]); 
         read(fd[READ], buf, MAX_BUF); // pipe에서 메세지 읽기
         printf("child got message : %s\n", buf);
     }
+
     exit(0);
 }
