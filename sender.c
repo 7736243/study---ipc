4d5fe0e083b6776c1b79b6c511a8c43c8589409e
7736243 <7736243@naver.com>
2021-10-07 10:53:51 +0900

    20211007 ipc(named_pipe(sender))

---
+++ sender.c
@@ -0,0 +1,31 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MSG_SIZE 80
+
+int main(){
+    char msg[MSG_SIZE];
+    int filedes;
+    int cnt;
+
+    if((filedes=open("./fifo", O_WRONLY))<0){
+        printf("fail to call open()\n");
+        exit(1);
+    }
+
+    for(cnt=0;cnt<3;cnt++){
+        printf("input a message : ");
+        gets(msg);
+        msg[MSG_SIZE]='\0';
+
+        if(write(filedes, msg, MSG_SIZE)==-1){
+            printf("fail to call write()\n");
+            exit(1);
+        }
+        sleep(1);
+    }
+}
